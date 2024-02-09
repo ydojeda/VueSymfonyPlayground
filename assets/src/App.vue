ba7a61743296e8f4129146bc5a2f43b4cbe4d4a5
ydojeda <yna.ojeda@asknice.ly>
2024-02-10 01:24:26 +1300

    initialise symfony-vue project

---
+++ assets/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <p>Hello World</p>
+</template>
+
+
+<script setup>
+
+</script>
+
+
+
+<style scoped>
+
+</style>
---
+++ assets/src/app.js
@@ -0,0 +1,6 @@
+import { createApp } from 'vue'
+// import the root component App from a single-file component.
+import App from './App.vue'
+
+const app = createApp(App)
+app.mount('#app')
